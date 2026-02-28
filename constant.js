390b43a38cdcaef4a0c861ba6dcaa47b6695b317
HitDoshi <hitdoshi2001@gmail.com@gmail.com>
2026-02-28 21:28:21 +0530

    Google OAuth

--- constant.js
+++ constant.js
@@ -1,5 +1,7 @@
 var API_URL = "https://learnit123.fun";
 
+var GOOGLE_CLIENT_ID = "802732338274-s3ckhe3hup9f8hhnjqpf6jn5t1vnt69n.apps.googleusercontent.com";
+
 let languageList = [
     { id: 1, name: "English", value: "en-US" },
     { id: 2, name: "French", value: "fr-CA" },
@@ -8,5 +10,5 @@ let languageList = [
     { id: 5, name: "German", value: "de-DE" },
     { id: 6, name: "Japanese", value: "ja-JP" },
     { id: 7, name: "Russian", value: "ru-RU" },
-    {id: 8, name: "Italian", value: "it-IT"},
+    { id: 8, name: "Italian", value: "it-IT" },
 ];
